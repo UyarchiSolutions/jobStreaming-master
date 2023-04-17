6c76103c906bfaf248d9c358102503de263edd1b
muthamizhselvan R <muthamizhyadav@gmail.com>
2023-04-17 13:41:54 +0530

    adsf

--- src/services/candidateDetails.service.js
+++ src/services/candidateDetails.service.js
@@ -2157,7 +2157,7 @@ const candidateSearch_front_page = async (id, body) => {
           experienceMatch,
           expMatch,
           advsearchMatch,
-          keySkillSearch
+          keySkillSearch,
         ],
       },
     },
@@ -2252,134 +2252,142 @@ const candidateSearch_front_page = async (id, body) => {
     { $limit: range },
   ]);
 
-let total  = await EmployerDetails.aggregate([
-  {
-    $sort: { createdAt: -1 },
-  },
-  {
-    $match: {
-      $or: allSearch,
+  let total = await EmployerDetails.aggregate([
+    {
+      $sort: { createdAt: -1 },
     },
-  },
-  {
-    $match: {
-      $and: experienceAnotherSearch,
+    {
+      $match: {
+        $or: allSearch,
+      },
     },
-  },
-  {
-    $match: {
-      $and: preferredindustrySearch,
+    {
+      $match: {
+        $and: experienceAnotherSearch,
+      },
     },
-  },
-  {
-    $match: {
-      $and: [
-        //  { jobortemplate: { $eq: 'job' } },
-        locationSearch,
-        salarySearch,
-        salary,
-        workmodeSearch,
-        educationSearch,
-        roleSearch,
-        departmentSearch,
-        freshnessSearch,
-        experienceMatch,
-        expMatch,
-        advsearchMatch,
-        keySkillSearch
-      ],
+    {
+      $match: {
+        $and: preferredindustrySearch,
+      },
     },
-  },
-  {
-    $lookup: {
-      from: 'employerregistrations',
-      localField: 'userId',
-      foreignField: '_id',
-      pipeline: [
-        {
-          $match: {
-            $and: [companytypeSearch, postedbySearch],
+    {
+      $match: {
+        $and: [
+          //  { jobortemplate: { $eq: 'job' } },
+          locationSearch,
+          salarySearch,
+          salary,
+          workmodeSearch,
+          educationSearch,
+          roleSearch,
+          departmentSearch,
+          freshnessSearch,
+          experienceMatch,
+          expMatch,
+          advsearchMatch,
+          keySkillSearch,
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: 'employerregistrations',
+        localField: 'userId',
+        foreignField: '_id',
+        pipeline: [
+          {
+            $match: {
+              $and: [companytypeSearch, postedbySearch],
+            },
           },
-        },
-      ],
-      as: 'employerregistrations',
+        ],
+        as: 'employerregistrations',
+      },
     },
-  },
-  {
-    $unwind: '$employerregistrations',
-  },
-  {
-    $lookup: {
-      from: 'candidatepostjobs',
-      localField: '_id',
-      foreignField: 'jobId',
-      pipeline: [
-        {
-          $match: { userId: { $eq: id } },
-        },
-      ],
-      as: 'candidatepostjobs',
+    {
+      $unwind: '$employerregistrations',
     },
-  },
-  {
-    $unwind: {
-      path: '$candidatepostjobs',
-      preserveNullAndEmptyArrays: true,
+    {
+      $lookup: {
+        from: 'candidatepostjobs',
+        localField: '_id',
+        foreignField: 'jobId',
+        pipeline: [
+          {
+            $match: { userId: { $eq: id } },
+          },
+        ],
+        as: 'candidatepostjobs',
+      },
     },
-  },
-  {
-    $lookup: {
-      from: 'jobroles',
-      localField: 'role',
-      foreignField: '_id',
-      as: 'jobroles',
+    {
+      $unwind: {
+        path: '$candidatepostjobs',
+        preserveNullAndEmptyArrays: true,
+      },
     },
-  },
-  {
-    $unwind: {
-      path: '$jobroles',
-      preserveNullAndEmptyArrays: true,
+    {
+      $lookup: {
+        from: 'jobroles',
+        localField: 'role',
+        foreignField: '_id',
+        as: 'jobroles',
+      },
     },
-  },
-  {
-    $project: {
-      keySkill: 1,
-      jobTittle: 1,
-      recruiterName: 1,
-      contactNumber: 1,
-      jobDescription: 1,
-      educationalQualification: 1,
-      salaryRangeFrom: 1,
-      salaryRangeTo: 1,
-      experienceFrom: 1,
-      experienceTo: 1,
-      interviewType: 1,
-      candidateDescription: 1,
-      salaryDescription: 1,
-      urltoApply: 1,
-      workplaceType: 1,
-      industry: 1,
-      preferedIndustry: 1,
-      jobLocation: 1,
-      employmentType: 1,
-      openings: 1,
-      date: 1,
-      expiredDate: 1,
-      date: 1,
-      roleCategory: 1,
-      time: 1,
-      companyType: '$employerregistrations.companyType',
-      mobileNumber: '$employerregistrations.mobileNumber',
-      contactName: '$employerregistrationscontactName',
-      email: '$employerregistrations.email',
-      name: '$employerregistrations.name',
-      appliedStatus: '$candidatepostjobs.approvedStatus',
-      role: '$jobroles.Job_role',
+    {
+      $unwind: {
+        path: '$jobroles',
+        preserveNullAndEmptyArrays: true,
+      },
     },
-  },
-]);
+    {
+      $project: {
+        keySkill: 1,
+        jobTittle: 1,
+        recruiterName: 1,
+        contactNumber: 1,
+        jobDescription: 1,
+        educationalQualification: 1,
+        salaryRangeFrom: 1,
+        salaryRangeTo: 1,
+        experienceFrom: 1,
+        experienceTo: 1,
+        interviewType: 1,
+        candidateDescription: 1,
+        salaryDescription: 1,
+        urltoApply: 1,
+        workplaceType: 1,
+        industry: 1,
+        preferedIndustry: 1,
+        jobLocation: 1,
+        employmentType: 1,
+        openings: 1,
+        date: 1,
+        expiredDate: 1,
+        date: 1,
+        roleCategory: 1,
+        time: 1,
+        companyType: '$employerregistrations.companyType',
+        mobileNumber: '$employerregistrations.mobileNumber',
+        contactName: '$employerregistrationscontactName',
+        email: '$employerregistrations.email',
+        name: '$employerregistrations.name',
+        appliedStatus: '$candidatepostjobs.approvedStatus',
+        role: '$jobroles.Job_role',
+      },
+    },
+  ]);
 
-  return { data: data, count: total.length };
+  let len = total.length;
+  let val = data.length;
+  let status;
+  if (val > len) {
+    status = true;
+  } else {
+    status = false;
+  }
+  return { data: data, count: total.length, status: status };
 };
 
 const recentSearch = async (userId) => {
