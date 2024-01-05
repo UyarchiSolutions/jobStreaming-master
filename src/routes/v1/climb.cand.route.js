4648e20eea15933ddbea9c6976de8c89225d864a
muthamizhyadav <muthamizhyadav@gmail.com>
2024-01-05 18:09:28 +0530

    asdf

--- src/routes/v1/climb.cand.route.js
+++ src/routes/v1/climb.cand.route.js
@@ -7,6 +7,6 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage }).single('resume');
 
 router.route('/').post(ClimbCandController.createClimbCand);
-router.route('/resume/upload')(upload, ClimbCandController.ResumeUploadCand);
+router.route('/resume/upload').post(upload, ClimbCandController.ResumeUploadCand);
 router.route('/:id').put(ClimbCandController.updateClimbCand);
 module.exports = router;
