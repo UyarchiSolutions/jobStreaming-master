f3036c946b3886b43a145a72d1c4b5df83a7a9e0
bharathi raja <bharathi@bharathis-MacBook-Pro.local>
2024-02-06 10:31:05 +0530

    d

--- src/services/volunteer.service.js
+++ src/services/volunteer.service.js
@@ -226,9 +226,11 @@ const CandidateIntrestUpdate = async (req) => {
     cand.interest_TECH = cand.interest_TECH + 1;
     cand = await AgriCandidate.findByIdAndUpdate({ _id: candId }, { $push: { techIntrest: volunteerId } }, { new: true });
   }
-  if (cand.interest_TECH <= 2 && cand.interest_HR <= 2) {
+  if (cand.interest_TECH >= 2 && cand.interest_HR >= 2) {
     cand.status = 'Waiting For Approval';
   }
+
+  console.log(cand)
   cand.save();
 
   await IntrestedCandidate.create({
