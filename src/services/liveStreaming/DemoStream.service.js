917ae21de17b1c831ac1cf819947bd71b9fce81a
bharathi raja <bharathi@bharathis-MacBook-Pro.local>
2024-02-14 14:18:55 +0530

    f

--- src/services/liveStreaming/DemoStream.service.js
+++ src/services/liveStreaming/DemoStream.service.js
@@ -175,7 +175,7 @@ const end_stream = async (req) => {
 
 
   let cand = await AgriCandidate.findById(token.candId);
-  const slots = await SlotBooking.find({ candId: cand._id, streamStatus: "Completed", rating: "Ratting Pending" }).count();
+  const slots = await SlotBooking.find({ candId: cand._id, streamStatus: "Completed", rating: "Rating Incomplete" }).count();
 
   if (slots == 2) {
     cand.status = "Completed";
