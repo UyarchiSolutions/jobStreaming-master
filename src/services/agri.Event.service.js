24b2b1bfdf09d56b358299712cf1c485598e881d
bharathi raja <bharathi@bharathis-MacBook-Pro.local>
2024-02-19 11:32:58 +0530

    d

--- src/services/agri.Event.service.js
+++ src/services/agri.Event.service.js
@@ -91,7 +91,7 @@ const slotDetailsAgriTch = async () => {
     {
       $group: {
         _id: { date: '$date' },
-        time: { $push: '$date' },
+        time: { $push: '$slot' },
       },
     },
     {
